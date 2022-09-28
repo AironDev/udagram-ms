af8a3ef9730e165ec00d4ec32d183f532ea0b3ac
Aaron Aniebiet <airondev@gmail.com>
2022-09-28 07:20:54 +0100

    deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a418025b0e0a5489eb7d1a1372c5d46d-141793100.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
